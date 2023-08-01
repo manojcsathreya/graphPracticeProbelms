befe643b28ac3df54693f8fa8c0b9e01c1561961
manojcsathreya <39308808+manojcsathreya@users.noreply.github.com>
2023-07-31 22:13:46 -0400

    commit

--- Detect Cycle in directed graph using DFS.py
+++ Detect Cycle in directed graph using DFS.py
@@ -1,15 +1,17 @@
-adj = {1:[2],2:[3], 3:[4], 4:[]}
+adj = {1:[2],2:[3,4], 3:[5], 4:[5], 5:[]}
 visit = set()
 pathVisit = set()
 
 def dfs(node,parent):
     visit.add(node)
+    pathVisit.add(node)
     for neighbour in adj[node]:
         if neighbour not in visit:
             if dfs(neighbour, node):
                 return True
-        elif parent != neighbour:
+        elif neighbour in pathVisit:
             return True
+    pathVisit.remove(node)
     return False
 
 print("No Cycle") if not dfs(1,-1) else print("Cycle")
